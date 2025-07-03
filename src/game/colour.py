0c85a24f114f76614c6a09e5aa4011561cc9be4b
Pradeep Kumar R <ravichandranpradeepkumar@gmail.com>
2025-07-04 01:35:37 +0400

    Added colour and direction of movement enums

---
+++ src/game/colour.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Colour(Enum):
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+    PURPLE = (128, 0, 128)
+    BLUE = (0, 0, 255)
+    LIGHT_GREY = (211, 211, 211)
+    TRANSPARENT_GREY = (200, 200, 200, 100)
+    ORANGE = (255, 165, 0)
+    YELLOW = (255, 255, 0)
---
+++ src/game/direction.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Direction(Enum):
+    RIGHT = 0
+    DOWN = 1
+    LEFT = 2
+    UP = 3
