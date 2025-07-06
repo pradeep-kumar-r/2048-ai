a888fb16c45cfa8e721863a894aa8ab08b141d7c
Pradeep Kumar R <ravichandranpradeepkumar@gmail.com>
2025-07-07 03:03:53 +0400

    Added all ai modules

--- src/ai/replay_buffer.py
+++ src/ai/replay_buffer.py
@@ -7,17 +7,17 @@ Transition = collections.namedtuple('Transition',
 
 
 class ReplayBuffer:
-    def __init__(self, capacity):
+    def __init__(self, capacity: int):
         self.memory = collections.deque([], maxlen=capacity)
 
-    def push(self, *args):
+    def push(self, *args) -> None:
         """Save a transition."""
         self.memory.append(Transition(*args))
 
-    def sample(self, batch_size):
+    def sample(self, batch_size: int) -> list:
         if len(self.memory) < batch_size:
             return self.memory
         return random.sample(self.memory, batch_size)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.memory)
